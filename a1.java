a56b8861844fe45cfb727ef3284564f885e34597
AyeshaM01 <mullaayesha7676@gmail.com>
2026-06-24 22:11:41 +0530

    1st code

---
+++ a1.java
@@ -0,0 +1,11 @@
+
+public class a1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int a = 4;
+        int b = 7;
+        System.out.println(a + b);
+          
+    }
+}
